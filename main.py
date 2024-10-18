81a2001636dfed7da0588a00bf2476f8744ec080
Shadowblued <drmf135@gmail.com>
2024-10-18 14:09:08 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/produtos")
+def produtos():
+    return {"produtos" : "todos"}
+
+@app.get("/")
+
+def read_root():
+
+    return {"Hello": "World"}
+
+
+
+def main():
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+
+
+if __name__ == "__main__":
+
+    main()
